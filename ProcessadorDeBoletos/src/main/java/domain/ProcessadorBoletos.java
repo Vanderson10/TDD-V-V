b39b9cc031b14ac7435c0a6c62035eec8dda73f9
andrezasantana <andreza.santana.ext@synchro.com.br>
2024-03-05 22:56:35 -0300

    Pagamento por boleto

--- ProcessadorDeBoletos/src/main/java/domain/ProcessadorBoletos.java
+++ ProcessadorDeBoletos/src/main/java/domain/ProcessadorBoletos.java
@@ -1,7 +1,9 @@
 package domain;
 
 import enumeration.StatusFatura;
+import enumeration.TipoPagamento;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,6 +27,7 @@ public class ProcessadorBoletos {
 
         for (Boleto boleto : boletos) {
             totalPago += boleto.getValorPago();
+            Pagamento pagamento = new Pagamento(boleto.getValorPago(), LocalDate.now(), TipoPagamento.BOLETO, boleto, fatura);
         }
 
         this.faturasProcessadas.add(fatura);

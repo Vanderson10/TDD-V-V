e645c18710b98535ea07d653ca51f7ddea38d208
Vanderson10 <araujovanderson223@gmail.com>
2024-04-03 18:37:12 -0300

    Testes Junit 5

--- SistemaReservaVoo/src/test/java/junit5Tests/functionalTests/SistemaVooTest.java
+++ SistemaReservaVoo/src/test/java/junit5Tests/functionalTests/SistemaVooTest.java
@@ -1,14 +1,19 @@
-package functionalTests;
+package junit5Tests.functionalTests;
 
-import org.junit.Before;
-import org.junit.Test;
+import junit5Tests.interfaces.CancelaVooTest;
+import junit5Tests.interfaces.PesquisaVooTest;
+import junit5Tests.interfaces.ReservaVooTest;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.sistema.reserva.voo.implementation.Cliente;
 import org.sistema.reserva.voo.implementation.Sistema;
 import org.sistema.reserva.voo.implementation.Voo;
 
+import static org.junit.Assert.assertTrue;
 
-import static org.junit.Assert.*;
-
+@DisplayName("Testes Funcionais para reserva, cancelamento e busca de voos")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class SistemaVooTest {
 
     private Voo voo;
@@ -17,7 +22,8 @@ public class SistemaVooTest {
 
     private Sistema sistema;
 
-    @Before
+    @BeforeEach
+    @DisplayName("Instância Objetos")
     public void CadastroVoo() {
         voo = new Voo("01/01/2024", "V001", 100.0, "Origem", "Destino", 300, "17h");
         sistema = new Sistema();
@@ -25,77 +31,68 @@ public class SistemaVooTest {
         clienteTest = new Cliente("Everton", 5, "123456789");
     }
 
-    @Test
+    @ReservaVooTest
+    @DisplayName("Reserva com 0 passageiros")
     public void testReservaVooZeroPassageiros() {
         Cliente cliente = new Cliente("Vanderson", 0, "123456789");
         cliente.reservarVoo(voo);
         assertTrue(cliente.getVoosReservados().size() != 0);
     }
 
-    @Test
+    @ReservaVooTest
+    @DisplayName("Reserva com -1 passageiros")
     public void testReservaVooNegativoPassageiros() {
         Cliente cliente = new Cliente("Vanderson", -1, "123456789");
         cliente.reservarVoo(voo);
         assertTrue(cliente.getVoosReservados().size() != 0);
     }
 
-    @Test
-    public void testReservaVooValidoPassageiros() {
-        Cliente cliente = new Cliente("Vanderson", 2, "123456789");
+    @ParameterizedTest
+    @ValueSource(ints = { 2, 250, 299, 300})
+    @DisplayName("Reserva com 2 passageiros")
+    public void testReservaVooValidoPassageiros(int passageiros) {
+        Cliente cliente = new Cliente("Vanderson", passageiros, "123456789");
         cliente.reservarVoo(voo);
         assertTrue(cliente.getVoosReservados().size() == 1);
     }
 
-    @Test
-    public void testReservaVoo250Passageiros() {
-        Cliente cliente = new Cliente("Vanderson", 250, "123456789");
-        cliente.reservarVoo(voo);
-        assertTrue(cliente.getVoosReservados().size() == 1);
-    }
-
-    @Test
-    public void testReservaVoo299Passageiros() {
-        Cliente cliente = new Cliente("Vanderson", 299, "123456789");
-        cliente.reservarVoo(voo);
-        assertTrue(cliente.getVoosReservados().size() == 1);
-    }
-
-    @Test
-    public void testReservaVoo300Passageiros() {
-        Cliente cliente = new Cliente("Vanderson", 300, "123456789");
-        cliente.reservarVoo(voo);
-        assertTrue(cliente.getVoosReservados().size() == 1);
-    }
-
-    @Test
+    @ReservaVooTest
+    @DisplayName("Reserva com 301 passageiros")
     public void testReservaVoo301Passageiros() {
         Cliente cliente = new Cliente("Vanderson", 301, "123456789");
         cliente.reservarVoo(voo);
         assertTrue(cliente.getVoosReservados().size() == 0);
     }
 
-    @Test
+    @CancelaVooTest
+    @DisplayName("Cancelamento com Código Errado")
     public void testCancelamentoCodigoInvalido() {
         clienteTest.reservarVoo(voo);
         clienteTest.cancelarReserva("CodigoErrado");
         assertTrue(clienteTest.getVoosReservados().size() == 1);
     }
 
-    @Test
+    @CancelaVooTest
+    @DisplayName("Cancelamento com Código Correto")
     public void testCancelamentoCodigoValido() {
         clienteTest.reservarVoo(voo);
         clienteTest.cancelarReserva(voo.getCodigo());
         assertTrue(clienteTest.getVoosReservados().size() == 0);
     }
 
-    @Test
+    @PesquisaVooTest
+    @Order(1)
+    @DisplayName("Busca existente no sistema")
     public void testBuscaVooExistente() {
         assertTrue(clienteTest.pesquisarVoos(sistema, "Origem", "Destino", "01/01/2024", 67));
     }
 
-    @Test
-    public void testBuscaVooInexistente() {
-        assertFalse(clienteTest.pesquisarVoos(sistema, "nada", "___", "756745", 6789));
+    @ParameterizedTest
+    @Order(2)
+    @ValueSource(strings = { "Origem"})
+    @DisplayName("Busca inexistente no sistema")
+    public void testBuscaVoos(String origem) {
+        assertTrue(clienteTest.pesquisarVoos(sistema, origem, "___", "756745", 6789));
     }
 
 }
---
+++ SistemaReservaVoo/src/test/java/testesUnitarios/VooTest.java
@@ -0,0 +1,40 @@
+package testesUnitarios;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import org.sistema.reserva.voo.implementation.Cliente;
+import org.sistema.reserva.voo.implementation.Voo;
+
+public class VooTest {
+
+    @Test
+    public void testIsDisponivel() {
+        Voo voo = new Voo("01/01/2024", "V001", 100.0, "Origem", "Destino", 100, "16h");
+        assertTrue(voo.isDisponivel());
+
+        // Reservando o voo
+        Cliente cliente = new Cliente("Vanderson", 2, "565675745");
+        voo.setCliente(cliente);
+        assertTrue(voo.isDisponivel());
+    }
+
+    @Test
+    public void testGetPrecoTotal() {
+        Voo voo = new Voo("01/01/2024", "V001", 100.0, "Origem", "Destino", 5, "17h");
+        assertEquals(500.0, voo.getPrecoTotal(), 0.001);
+
+        // Modificando o preço do voo
+        voo.setPreco(120.0);
+        assertEquals(600.0, voo.getPrecoTotal(), 0.001);
+    }
+
+    @Test
+    public void testImprimirDetalhes() {
+        Voo voo = new Voo("01/01/2024", "V001", 100.0, "Origem", "Destino", 100, "20h");
+        Cliente cliente = new Cliente("João", 1, "6476347843");
+        voo.setCliente(cliente);
+
+        // Verifica se a impressão não lança exceções
+        voo.imprimirDetalhes();
+    }
+}
